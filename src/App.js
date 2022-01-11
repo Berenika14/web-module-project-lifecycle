8923d4c90942a6adf9800e57bf4d86a9c2109611
Berenika Ahmetaj <ahmetajberenika@gmail.com>
2022-01-11 17:17:48 -0500

    Created the dom based on the template and the data endpoints

--- src/App.js
+++ src/App.js
@@ -1,10 +1,57 @@
-import React from 'react';
+import React from "react";
 
 class App extends React.Component {
   render() {
-    return(<div>
-      Github Card
-    </div>);
+    return (
+      <div>
+        <h1>GITHUB INFO</h1>
+        <form>
+          <input placeholder="Github handler" />
+          <button>Search</button>
+        </form>
+        <div id="UserCard">
+          <img
+            width="200px"
+            src="https://avatars.githubusercontent.com/u/91492156?v=4"
+          />
+          <a href="https://github.com/Berenika14">
+            <h3>Berenika Ahmetaj</h3>
+          </a>
+          <p>(Software Developer)</p>
+          <p>Total Repos: 35 </p>
+          <p>Total followers:16</p>
+        </div>
+        <div id="followers">
+          <div className="follower">
+            <img
+              width="200px"
+              src="https://avatars.githubusercontent.com/u/18543621?v=4"
+            />
+            <a href="https://github.com/JCSime">
+              <p>JCSime</p>
+            </a>
+          </div>
+          <div className="follower">
+            <img
+              width="200px"
+              src="https://avatars.githubusercontent.com/u/18543621?v=4"
+            />
+            <a href="https://github.com/JCSime">
+              <p>JCSime</p>
+            </a>
+          </div>
+          <div className="follower">
+            <img
+              width="200px"
+              src="https://avatars.githubusercontent.com/u/18543621?v=4"
+            />
+            <a href="https://github.com/JCSime">
+              <p>JCSime</p>
+            </a>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
